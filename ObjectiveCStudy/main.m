07f01f7893611dc24f5716f4f99cabd718f31a3a
习雄辉 <study_blogs@163.com>
2019-04-01 18:08:35 +0800

    Initial Commit

---
+++ ObjectiveCStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCStudy
+//
+//  Created by 习雄辉 on 2019/4/1.
+//  Copyright © 2019年 习雄辉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
